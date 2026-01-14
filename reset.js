25d3fdddbe948b92efc1ac7e79c2ae3472df49d5
FabRieHFU <fri45001@stud.hs-furtwangen.de>
2026-01-14 12:50:15 +0100

    12:50_aenderungen

---
+++ reset.js
@@ -0,0 +1,5 @@
+// Dieses Script in der Browser-Konsole ausführen um alles zu löschen
+localStorage.clear();
+sessionStorage.clear();
+console.log('✅ Cache komplett gelöscht!');
+console.log('Bitte Seite neu laden (F5 oder Cmd+R)');
